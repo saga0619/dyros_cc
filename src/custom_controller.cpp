b13aa769e260d8798e393971f2971fc638f7095f
saga0619 <june992@gmail.com>
2020-03-04 16:25:10 +0900

    ADD example for wholebody controller

--- src/custom_controller.cpp
+++ src/custom_controller.cpp
@@ -23,12 +23,28 @@ void CustomController::computeSlow()
     //rd_.link_[Right_Foot]
 
     //ControlVal_=
+
+    wbc_.set_contact(rd_, 1, 1);
+
+    int task_number = 6;
+    rd_.J_task.setZero(task_number, MODEL_DOF_VIRTUAL);
+    rd_.f_star.setZero(task_number);
+
+    rd_.J_task = rd_.link_[Pelvis].Jac;
+
+    rd_.link_[Pelvis].x_desired = tc.ratio * rd_.link_[Left_Foot].xpos + (1.0 - tc.ratio) * rd_.link_[Right_Foot].xpos;
+    rd_.link_[Pelvis].x_desired(2) = tc.height + tc.ratio * rd_.link_[Left_Foot].xpos(2) + (1.0 - tc.ratio) * rd_.link_[Right_Foot].xpos(2);
+    rd_.link_[Pelvis].Set_Trajectory_from_quintic(rd_.control_time_, tc.command_time, tc.command_time + tc.traj_time);
+
+    rd_.f_star = wbc_.getfstar6d(rd_, Pelvis);
+
+    ControlVal_ = wbc_.task_control_torque_QP2(rd_, rd_.J_task, rd_.f_star);
+
 }
 
 void CustomController::computeFast()
-{   
+{
     if (tc.mode == 10)
     {
-
     }
 }
