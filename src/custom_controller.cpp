be27d790cb934173f4263604076a236748e53137
saga0619 <june992@gmail.com>
2020-03-04 16:29:00 +0900

    Add controller mode

--- src/custom_controller.cpp
+++ src/custom_controller.cpp
@@ -16,30 +16,32 @@ void CustomController::taskCommandToCC(TaskCommand tc_)
 
 void CustomController::computeSlow()
 {
-    //rd_.control_time_; current time
-    //rd_.link_[Right_Foot].Jac : current rightfoot jac
-    //rd_.q_dot_ : current q velocity
+    if (tc.mode == 10)
+    {
+        //rd_.control_time_; current time
+        //rd_.link_[Right_Foot].Jac : current rightfoot jac
+        //rd_.q_dot_ : current q velocity
 
-    //rd_.link_[Right_Foot]
+        //rd_.link_[Right_Foot]
 
-    //ControlVal_=
+        //ControlVal_=
 
-    wbc_.set_contact(rd_, 1, 1);
+        wbc_.set_contact(rd_, 1, 1);
 
-    int task_number = 6;
-    rd_.J_task.setZero(task_number, MODEL_DOF_VIRTUAL);
-    rd_.f_star.setZero(task_number);
+        int task_number = 6;
+        rd_.J_task.setZero(task_number, MODEL_DOF_VIRTUAL);
+        rd_.f_star.setZero(task_number);
 
-    rd_.J_task = rd_.link_[Pelvis].Jac;
+        rd_.J_task = rd_.link_[Pelvis].Jac;
 
-    rd_.link_[Pelvis].x_desired = tc.ratio * rd_.link_[Left_Foot].xpos + (1.0 - tc.ratio) * rd_.link_[Right_Foot].xpos;
-    rd_.link_[Pelvis].x_desired(2) = tc.height + tc.ratio * rd_.link_[Left_Foot].xpos(2) + (1.0 - tc.ratio) * rd_.link_[Right_Foot].xpos(2);
-    rd_.link_[Pelvis].Set_Trajectory_from_quintic(rd_.control_time_, tc.command_time, tc.command_time + tc.traj_time);
+        rd_.link_[Pelvis].x_desired = tc.ratio * rd_.link_[Left_Foot].xpos + (1.0 - tc.ratio) * rd_.link_[Right_Foot].xpos;
+        rd_.link_[Pelvis].x_desired(2) = tc.height + tc.ratio * rd_.link_[Left_Foot].xpos(2) + (1.0 - tc.ratio) * rd_.link_[Right_Foot].xpos(2);
+        rd_.link_[Pelvis].Set_Trajectory_from_quintic(rd_.control_time_, tc.command_time, tc.command_time + tc.traj_time);
 
-    rd_.f_star = wbc_.getfstar6d(rd_, Pelvis);
+        rd_.f_star = wbc_.getfstar6d(rd_, Pelvis);
 
-    ControlVal_ = wbc_.task_control_torque_QP2(rd_, rd_.J_task, rd_.f_star);
-
+        ControlVal_ = wbc_.task_control_torque_QP2(rd_, rd_.J_task, rd_.f_star);
+    }
 }
 
 void CustomController::computeFast()
